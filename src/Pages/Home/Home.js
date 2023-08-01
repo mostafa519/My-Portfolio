a3af318f7079ae2cf502fbe1fb58ef16fdf07011
Mustafa Ali <engmostafa51999@gmail.com>
2023-08-02 02:58:28 +0300

    editting

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
    <> 
     <h2 className='homeedit pt-5' ><Link to={"/movies"}>Movies Page</Link></h2>
-   <Movies></Movies>
+   {/* <Movies></Movies> */}
    <h2 className='homeedit pt-5' ><Link to={"/product"}>Products Page</Link></h2>
    <Product></Product>
    <h2 className='homeedit pt-5' ><Link to={"/articles"}>Articles Page</Link></h2>
